7e29b44ec17ac1ca206ebaf0f1856166d1acaf76
Razzmatazz146 <denispletourneau@hotmail.com>
2024-04-11 17:01:24 -0400

    regex fixed dont @ me

--- client/src/login.tsx
+++ client/src/login.tsx
@@ -50,7 +50,7 @@ function login() {
     // };
 
     const validateEmail = (email: any) => {
-        const reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const reg = /^[A-Za-z0-9^@]+@[A-Za-z0-9^@]+.[A-Za-z0-9^@]+$/;
         return reg.test(String(email).toLowerCase());
     };
 
